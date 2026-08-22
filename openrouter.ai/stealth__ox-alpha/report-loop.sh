d5b3aafa362811f9f42329760c675b396489404b
Soonki Ji <soonki.ji@gmail.com>
2026-08-22 15:39:04 -0400

    Update report.results.txt every 10 mins.

--- openrouter.ai/stealth__ox-alpha/report-loop.sh
+++ openrouter.ai/stealth__ox-alpha/report-loop.sh
@@ -8,5 +8,6 @@ do
 	if [ -n "$(git status --porcelain -- report.results.txt)" ]; then
 		git add report.results.txt && git commit -m "Update report.results.txt" && git push
 	fi
-	sleep 1800
+	echo + sleep 600
+	sleep 600
 done

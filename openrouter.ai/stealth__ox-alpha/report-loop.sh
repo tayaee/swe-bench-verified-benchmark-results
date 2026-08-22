6d99db422be047bab0ba1743ca382d64b7b2aa9e
Soonki Ji <soonki.ji@gmail.com>
2026-08-22 04:51:14 -0400

    Update report-loop.sh

--- openrouter.ai/stealth__ox-alpha/report-loop.sh
+++ openrouter.ai/stealth__ox-alpha/report-loop.sh
@@ -2,9 +2,9 @@
 while :
 do
 	./eval.sh
-	./report.sh | tee results.txt
+	./report.sh | tee report.results.txt
 	if [ -n "$(git status --porcelain -- results.txt)" ]; then
-		git add results.txt && git commit -m "Update results.txt" && git push
+		git add report.results.txt && git commit -m "Update report.results.txt" && git push
 	fi
 	sleep 3600
 done
